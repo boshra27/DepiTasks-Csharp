1651f766268596a08c10404da643466f8da9377d
Boshra <215034432+boshra27@users.noreply.github.com>
2026-01-31 20:38:47 +0200

    task02 and task03

---
+++ Csharp02/Csharp02/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.IO;
+
+namespace Csharp02
+{
+    class program
+    {
+        static void Main()
+        {
+            #region Problem: single and multiline comments
+            //single line is used to comment only one line while multi - line comment
+            //is used for multiple lines
+            //int x = 10;
+            /*int y = 20;
+            int sum = x + y;
+            Console.WriteLine(sum); 
+            */
+            #endregion
+
+            #region Question: comment and uncomment shortcut
+            // ctrl,k,c and ctrl,k,u
+            #endregion
+
+            #region Question: runtime vs logical error
+            /*runtime error accours when the program is running like division by zero
+            while logical error is program not behaving as intended 
+            */
+            #endregion
+
+            #region Problem: variables with proper naming coventions
+            //string FullName;
+            //int Age;
+            //decimal Salary;
+            //bool IsStudent;
+
+            #endregion
+
+            #region Question: following naming coventions
+            // because it enhances code readability, maintainability and team collaboration
+            #endregion
+
+            #region Problem: reference type
+            //int[] X;
+            //int[] Y= { 1, 2, 3 };
+
+            //X = Y;
+            //for (int i = 0; i < X.Length; i++) {
+            //Console.WriteLine(X[i]);
+            //}
+            #endregion
+
+            #region Question: reference vs value types in memory allocation
+            // reference type is stored in heap while value type is in stack
+            #endregion
+
+            #region Problem: simple calculator
+            //int X = 15;
+            //int Y = 4;
+
+            //int Sum = X + Y;
+            //int Diff = X - Y;
+            //int Prod = X * Y;
+            //float Div = X / Y;
+            //int Remainder = X % Y;
+            #endregion
+
+            #region Qustion: output
+            // 2, because this calculates the remainder and since a is smaller than b it
+            // return the value of a
+            #endregion
+
+            #region Problem: check
+            //int X = 4;
+
+            //if (X > 10 && X % 2 == 0) {
+            //    Console.WriteLine("X is greater than 10 and even.");
+            //}
+            #endregion
+
+            #region Question: logical vs bitwise AND
+            // && operates on the boolean value of the entire expression and uses short-circuit evaluation
+            // & operates on individual bits of integer data types always evaluate both operands
+            #endregion
+
+            #region Problem: casting
+            //double X = double.Parse(Console.ReadLine());
+            //// connot implicitly cast double to int
+            //int IntX = (int)X;
+            //Console.WriteLine(IntX);
+
+            #endregion
+
+            #region Question: Required explicit casting
+            // because the converion is a narrowing convertion and data loss might occur 
+            #endregion
+
+            #region Problem: program
+            //Console.WriteLine("Enter your age: ");
+            //int Age = int.Parse(Console.ReadLine());
+
+            //if (Age < 0)
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            #endregion
+
+            #region Question: exceptions
+            // System.Formatexception, we can handle it using tryparse instead
+            #endregion
+
+            #region Problem: prefix vs postfix
+            //int X = 5;
+            //Console.WriteLine(X++);//5
+            //Console.WriteLine(++X);//7
+            #endregion
+
+            #region Question: value of X
+            // 7, because we added 1 to x twice, once in prefix and the other in postfix
+            #endregion
+
+            #region Part02: 2
+            /* compiled languages translates code to machine code as a whole while interpreted languages translates it
+             code line by line C# is considered a compiled language
+            */
+            #endregion
+
+            #region Part02: 3
+            /* implicit casting is done automatically and does not need special synatx
+               explicit casting is done manually and needs a type operator
+               parse is a method inside the data types definition
+               Convert is a class containing the parse methods
+             */
+            #endregion
+        }
+    }
+}
---
+++ Csharp03/Csharp03/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Csharp03
+{
+    class Program
+    {
+        static void Main() 
+        {
+            #region Problem: String to Int
+            /*try
+            {
+                int num = int.Parse(Console.ReadLine());
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input");
+                throw;
+            }*/
+
+            #endregion
+
+            #region Question: Parse vs Convert
+            //parse throws a ArgumentNull exception while Convert returns 0
+            #endregion
+
+            #region Problem: TryParse
+
+            /*bool success = int.TryParse(Console.ReadLine(),out int parsedNum);
+
+            if (success) 
+            {
+                Console.WriteLine(parsedNum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Format");
+            }*/
+            #endregion
+
+            #region Question: Tryparse over Parse
+            // To be able to handle exceptions in case of occurring when user input is invalid 
+            #endregion
+
+            #region Problem: Object
+            //object b1 = new object();
+            //b1 = "lsdfj";
+            //Console.WriteLine(b1.GetHashCode());
+            //b1 = 11.2;
+            //Console.WriteLine(b1.GetHashCode());
+
+            #endregion
+
+            #region Question: GEtHashCode()
+            //It's used for performance optimization in data structures that uses hash tables 
+            #endregion
+
+            #region Problem: Changing references
+            //object b1 = new object();
+            //b1 = 1;
+            //object b2 = b1;
+            //b1 = 2;
+            //Console.WriteLine(b2);
+
+            #endregion
+
+            #region Question: reference equality
+            //It is establishes identity even if two objects have 
+            //same data they're not reference equal if they're not the same instance
+            #endregion
+
+
+            #region Problem: Concatenating
+            //string s1 = "Hello, ";
+            //Console.WriteLine(s1.GetHashCode());
+            //s1 += "Hi Willy";
+            //Console.WriteLine(s1.GetHashCode());
+            #endregion
+
+            #region Question: immutable string
+            // because It's made of an array of characters 
+            #endregion
+
+            #region Problem: StringBuilder GetHashCode
+
+            //StringBuilder sb = new StringBuilder("Hi Willy");
+            //Console.WriteLine($"{sb.GetHashCode()}");
+
+            //sb.Append(", how are you?");
+            //Console.WriteLine($"{sb.GetHashCode()}");
+
+            #endregion
+
+            #region Question: StringBuilder address the inefficiencies of string
+            //It doesn't create a new object it changes the one we're using.
+            #endregion
+
+            #region Question: Large-scale string modificatons
+            //Because it won't fill the heap with objects with no reference 
+            #endregion
+
+            #region Problem: concatenation methods
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int sum = num1 + num2;
+
+            //Console.WriteLine("Sum is"+ num1 +"+"+ num2 + "=" + sum);
+            //Console.WriteLine("Sum is {0} + {1} = {2}",num1, num2, sum);
+            //Console.WriteLine($"Sum is {num1} + {num2} = {sum}");
+            #endregion
+
+            #region Question: most used string formatting method
+            //String interpolation because it doesn't create new object for
+            //each concatenation and it's better for readability.
+            #endregion
+
+            #region Problem: StringBuilder
+            StringBuilder sb = new StringBuilder("hello world");
+            sb.Append("!");
+            sb.Replace("world", "everyone");
+            sb.Insert(6, "there");
+            sb.Remove(0, 6);
+
+            #endregion
+
+            #region Question: SringBuilder design
+            //Stingbuilder is design to overcome immutability in string using
+            //an internal buffer to handle changes in place.
+            #endregion
+
+
+            //Part02
+            #region 2 
+            //enumeration is a value type used to define named integral constants.
+            //Used when variable can only take one value from a set of values.
+            //Examples: System.DayOfWeek, System.Net.HttpStatusCode, System.IO.FileMode
+            #endregion
+
+            #region 3
+            //string is used in simple joins 2-4 concatenation and outside of loops.
+            //StringBuilder is used for complex strings with non-constant values and used in loops.
+            #endregion
+        }
+    }
+}
